b1ddd6da91d4964294b76fa93f472d5144f39ed9
LeonardOrme29 <leonardo.ormeno@unmsm.edu.pe>
2025-05-31 13:45:24 -0500

    docker and feat dish api

--- routers/dish.py
+++ routers/dish.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from models.dish_model import DishModel
-from schemas.dish_schema import Dish
+from schemas.dish_schema import Dish,DishResponse
 from database import get_db
 
 router = APIRouter()
 
-@router.get("/dishes", response_model=list[Dish])
+@router.get("/dishes", response_model=list[DishResponse])
 def get_dishes(db: Session = Depends(get_db)):
     return db.query(DishModel).all()
+
+
+@router.post("/dishes/upload", response_model=DishResponse)
+def upload_dish(dish: Dish, db: Session = Depends(get_db)):
+    new_dish = DishModel(
+        name=dish.name,
+        category=dish.category,
+        url_image=dish.url_image
+    )
+    db.add(new_dish)
+    db.commit()
+    db.refresh(new_dish)
+    return new_dish
--- schemas/dish_schema.py
+++ schemas/dish_schema.py
@@ -2,10 +2,15 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Dish(BaseModel):
-    id: int
     name: str
     category: str
     url_image: Optional[str] = None
 
     class Config:
         orm_mode = True
+
+class DishResponse(BaseModel):
+    id: int
+    name: str
+    category: str
+    url_image: str
